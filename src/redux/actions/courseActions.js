c9182f542945dc6e22efde9571aa031b8abd6077
ma7ammad <kakadumdum@yandex.com>
2020-07-22 20:34:01 +0100

    introduced actionTypes.js to hold all the magic strings constants and implemented in App

--- src/redux/actions/courseActions.js
+++ src/redux/actions/courseActions.js
@@ -1,5 +1,6 @@
+import * as types from "./actionTypes";
 export function createCourse(course) {
-  return { type: "CREATE_COURSE", course }; //object shorthand syntax
+  return { type: types.CREATE_COURSE, course }; //object shorthand syntax
 
   // return { type: "CREATE_COURSE", course: course };
 }
