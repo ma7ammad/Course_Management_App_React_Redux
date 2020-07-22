2b6904d00b9cfbcba6614186d1fb8a521d4e977c
ma7ammad <kakadumdum@yandex.com>
2020-07-22 12:10:37 +0100

    added a form to add new courses to the CoursesPage: submit button not activated yet

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,8 +1,36 @@
 import React from "react";
 
 class CoursesPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      course: {
+        title: "",
+      },
+    };
+
+    // this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange = (event) => {
+    const course = { ...this.state.course, title: event.target.value };
+    this.setState({ course: course }); // this.state({ course }); will suffice
+  };
+
   render() {
-    return <h2>Courses</h2>;
+    return (
+      <form>
+        <h2>Courses</h2>
+        <h3>Add Course</h3>
+        <input
+          type="text"
+          onChange={this.handleChange}
+          value={this.state.course.title}
+        />
+
+        <input type="submit" value="Save" />
+      </form>
+    );
   }
 }
 
