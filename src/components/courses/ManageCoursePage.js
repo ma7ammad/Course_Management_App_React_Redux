e01f8fc9ca3ad88f75b5ca8c90b3800041c4a6a4
ma7ammad <kakadumdum@yandex.com>
2020-08-02 12:02:07 +0100

    display 'Course saved' message using toastify

--- src/components/courses/ManageCoursePage.js
+++ src/components/courses/ManageCoursePage.js
@@ -6,6 +6,7 @@ import PropTypes from "prop-types";
 import CourseForm from "./CourseForm";
 import { newCourse } from "../../../tools/mockData";
 import Spinner from "../common/Spinner";
+import { toast } from "react-toastify";
 ///////////////////////////////////////////////////////////////////////////////////////////////
 //component
 
@@ -60,6 +61,7 @@ function ManageCoursePage(props) {
     event.preventDefault();
     setSaving(true);
     saveCourse(course).then(() => {
+      toast.success("Course Saved");
       history.push("/courses");
     });
   }
